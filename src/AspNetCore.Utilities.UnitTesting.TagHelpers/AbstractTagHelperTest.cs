6e6f14b802678f8f0bf9282fc09b30e778c38d06
Matt Sieker <matt@siekernet.com>
2022-07-14 19:32:49 -0500

    Misc changes

--- src/AspNetCore.Utilities.UnitTesting.TagHelpers/AbstractTagHelperTest.cs
+++ src/AspNetCore.Utilities.UnitTesting.TagHelpers/AbstractTagHelperTest.cs
@@ -2,57 +2,60 @@
 using AngleSharp.Dom;
 using AngleSharp.Html;
 using ICG.AspNetCore.Utilities.UnitTesting.TagHelpers.FromFramework;
+using System.ComponentModel;
+using System.Linq.Expressions;
 
 namespace ICG.AspNetCore.Utilities.UnitTesting.TagHelpers;
 
+/// <summary>
+///     Base class for tag helper tests. Contains <see cref="VerifyTagHelper(Microsoft.AspNetCore.Razor.TagHelpers.TagHelperOutput,System.Action{AngleSharp.Dom.INodeList}?)"/>
+/// </summary>
 public abstract class BaseTagHelperTest
 {
-    public static TagHelperContext MakeTagHelperContext(string tagName = "div", TagHelperAttributeList? attributes = null)
-    {
-        attributes ??= new TagHelperAttributeList();
-
-        return new TagHelperContext(
-            tagName,
-            attributes,
-            new Dictionary<object, object>(),
-            Guid.NewGuid().ToString("N"));
-    }
-
-    public static TagHelperOutput MakeTagHelperOutput(
-        string tagName,
-        TagHelperAttributeList? attributes = null,
-        HtmlString? childContent = null)
-    {
-        attributes ??= new TagHelperAttributeList();
-
-        return new TagHelperOutput(
-            tagName,
-            attributes,
-            (useCachedResult, encoder) =>
-            {
-                var tagHelperContent = new DefaultTagHelperContent();
-                tagHelperContent.SetHtmlContent(childContent);
-                return Task.FromResult<TagHelperContent>(tagHelperContent);
-            });
-    }
-
+    /// <summary>
+    ///     Verifies the output of a tag helper
+    /// </summary>
+    /// <param name="output">The output of a tag helper</param>
+    /// <param name="action">A callback containing the <see cref="INodeList"/> from AngleSharp's parsing of the generated Html. Can be null</param>
+    /// <returns>A <see cref="SettingsTask"/> to await</returns>
     public virtual SettingsTask VerifyTagHelper(TagHelperOutput output, Action<INodeList>? action) => Verify(output.Render())
         .UseExtension("html")
         .ScrubEmptyLines()
         .PrettyPrintHtml(action);
 
+    /// <summary>
+    ///     Verifies the output of a tag helper
+    /// </summary>
+    /// <param name="output">The output of a tag helper</param>
+    /// <returns>A <see cref="SettingsTask"/> to await</returns>
     public virtual SettingsTask VerifyTagHelper(TagHelperOutput output) => VerifyTagHelper(output, null);
 }
 
+/// <summary>
+///     Base class for helper tests that will write the tag helper output to the xUnit logger.
+/// </summary>
 public abstract class LoggingTagHelperTest : BaseTagHelperTest
 {
+    /// <summary>
+    ///     The test output helper provided by xUnit
+    /// </summary>
     protected ITestOutputHelper Output { get; }
 
+    /// <summary>
+    ///     Creates an instance of the <see cref="LoggingTagHelperTest"/>
+    /// </summary>
+    /// <param name="output">The <see cref="ITestOutputHelper"/> injected by xUnit</param>
     protected LoggingTagHelperTest(ITestOutputHelper output)
     {
         Output = output;
     }
 
+    /// <summary>
+    ///     Verifies the output of a tag helper. Writes the prettified Html of the tag output
+    ///     to the xUnit logger.
+    /// </summary>
+    /// <param name="output">The output of a tag helper</param>
+    /// <returns>A <see cref="SettingsTask"/> to await</returns>
     public override SettingsTask VerifyTagHelper(TagHelperOutput output)
     {
         var hasWrittenOutput = false;
@@ -75,49 +78,66 @@ public abstract class LoggingTagHelperTest : BaseTagHelperTest
     }
 }
 
+/// <summary>
+///     Base class for tag helper tests involving the model binder, including validation and display attributes.
+/// </summary>
+/// <typeparam name="TTagHelper">The tag helper to test</typeparam>
+/// <typeparam name="TModel">The model to test with</typeparam>
 public abstract class ModelTagHelperTest<TTagHelper, TModel> : LoggingTagHelperTest where TModel : new()
 {
+    /// <summary>
+    ///     Creates an instance of the <see cref="ModelTagHelperTest{TTagHelper,TModel}"/>
+    /// </summary>
+    /// <param name="output">The <see cref="ITestOutputHelper"/> injected by xUnit</param>
     protected ModelTagHelperTest(ITestOutputHelper output) : base(output)
     {
     }
 
-    internal abstract TTagHelper TagHelperFactory(IHtmlGenerator htmlGenerator, ModelExpression modelExpression, ViewContext viewContext);
+    /// <summary>
+    /// Factory method to generate the tag helper.
+    /// </summary>
+    /// <param name="htmlGenerator">The <see cref="IHtmlGenerator"/> instance to provide the tag helper</param>
+    /// <param name="modelExpression">The <see cref="ModelExpression"/> to provide the tag helper</param>
+    /// <param name="viewContext">The <see cref="ViewContext"/> to provide the </param>
+    /// <returns></returns>
+    protected abstract TTagHelper TagHelperFactory(IHtmlGenerator htmlGenerator, ModelExpression modelExpression, ViewContext viewContext);
 
-    protected TTagHelper GetTagHelper(
-        IHtmlGenerator htmlGenerator,
-        object model,
-        string propertyName,
-        IModelMetadataProvider? metadataProvider = null)
+    protected TTagHelper GetTagHelper<TProperty>(TModel model, Expression<Func<TModel, TProperty>> property)
     {
-        return GetTagHelper(
-            htmlGenerator,
-            container: new TModel(),
-            containerType: typeof(TModel),
-            model: model,
-            propertyName: propertyName,
-            expressionName: propertyName,
-            metadataProvider: metadataProvider);
+        var name = ((MemberExpression)property.Body).Member.Name;
+        object propValue = property.Compile().Invoke(model);
+        var metadataProvider = new TestModelMetadataProvider();
+        var htmlGenerator = new TestableHtmlGenerator(metadataProvider);
+
+        return GetTagHelper(htmlGenerator, model, propValue, name);
     }
 
+    /// <summary>
+    /// Gets a tag helper instance configured as if using the asp-for attribute
+    /// </summary>
+    /// <param name="htmlGenerator">The <see cref="IHtmlGenerator"/> to use</param>
+    /// <param name="model">An instance of the <typeparamref name="TModel"/> to use</param>
+    /// <param name="propertyName">The property name from the model</param>
+    /// <param name="metadataProvider">A metadata provider to use, optional</param>
+    /// <returns>A tag helper created by <see cref="TagHelperFactory"/></returns>
     protected TTagHelper GetTagHelper(
         IHtmlGenerator htmlGenerator,
-        object container,
-        Type containerType,
+        TModel container,
         object model,
         string propertyName,
-        string expressionName,
         IModelMetadataProvider? metadataProvider = null)
     {
         metadataProvider ??= new TestModelMetadataProvider();
 
-        var containerExplorer = metadataProvider.GetModelExplorerForType(containerType, container);
+        var containerExplorer = metadataProvider.GetModelExplorerForType(typeof(TModel), model);
 
-        var propertyMetadata = metadataProvider.GetMetadataForProperty(containerType, propertyName);
+        var propertyMetadata = metadataProvider.GetMetadataForProperty(typeof(TModel), propertyName);
         var modelExplorer = containerExplorer.GetExplorerForExpression(propertyMetadata, model);
 
-        var modelExpression = new ModelExpression(expressionName, modelExplorer);
-        var viewContext = TestableHtmlGenerator.GetViewContext(container, htmlGenerator, metadataProvider);
+        var modelExpression = new ModelExpression(propertyName, modelExplorer);
+        var viewContext = TestableHtmlGenerator.GetViewContext(model, htmlGenerator, metadataProvider);
 
         return TagHelperFactory(htmlGenerator, modelExpression, viewContext);
+
     }
 }
--- src/AspNetCore.Utilities.UnitTesting.TagHelpers/TagHelperExtensions.cs
+++ src/AspNetCore.Utilities.UnitTesting.TagHelpers/TagHelperExtensions.cs
@@ -3,18 +3,53 @@
 namespace ICG.AspNetCore.Utilities.UnitTesting.TagHelpers;
 #nullable enable
 
+/// <summary>
+/// Extension methods for tag helpers
+/// </summary>
 public static class TagHelperExtensions
 {
-    public static async Task<TagHelperOutput> Render(this TagHelper helper, string tagName = "div", HtmlString childContent = null)
+    /// <summary>
+    ///     Renders a tag helper to a <see cref="TagHelperOutput"/>
+    /// </summary>
+    /// <param name="helper">The tag helper to render</param>
+    /// <param name="tagName">The tag name that the tag helper is for</param>
+    /// <param name="attributes">Attributes for the tag helper</param>
+    /// <param name="childContent">Child content of the tag helper</param>
+    /// <returns>A task returning a <see cref="TagHelperOutput"/></returns>
+    public static async Task<TagHelperOutput> Render(this TagHelper helper, string tagName = "div", TagHelperAttributeList? attributes = null, HtmlString? childContent = null)
     {
-        var context = BaseTagHelperTest.MakeTagHelperContext();
-        var output = BaseTagHelperTest.MakeTagHelperOutput("", childContent: childContent);
+        attributes ??= new TagHelperAttributeList();
+
+        var context = new TagHelperContext(
+            tagName,
+            attributes,
+            new Dictionary<object, object>(),
+            Guid.NewGuid().ToString("N"));
+
+        var output = new TagHelperOutput(
+            tagName,
+            attributes,
+            (useCachedResult, encoder) =>
+            {
+                var tagHelperContent = new DefaultTagHelperContent();
+                return Task.FromResult<TagHelperContent>(tagHelperContent.SetHtmlContent(childContent));
+            });
 
         await helper.ProcessAsync(context, output);
 
         return output;
     }
 
+    /// <summary>
+    ///     Renders tag helper output to a string.
+    /// </summary>
+    /// <param name="tagOutput">The output of a tag helper</param>
+    /// <remarks>
+    ///     Since this method uses the <see cref="HtmlTestEncoder"/> from the framework,
+    ///     any strings that will be HTML encoded before being sent to the client will be
+    ///     wrapped in <code>HtmlEncode[[]]</code>.
+    /// </remarks>
+    /// <returns>A string of the rendered tag helper.</returns>
     public static string Render(this TagHelperOutput tagOutput)
     {
         var writer = new StringWriter();
@@ -24,10 +59,16 @@ public static class TagHelperExtensions
         return writer.ToString();
     }
 
+    /// <summary>
+    ///     Asserts that the output of a tag helper references a specified CSS class
+    /// </summary>
+    /// <param name="output">The tag helper output</param>
+    /// <param name="className">The class name</param>
     public static void AssertContainsClass(this TagHelperOutput output, string className)
     {
-
         var tagClasses = output.Attributes["class"].Value.ToString()?.Split(' ');
+        Assert.NotNull(tagClasses);
+        Assert.NotEmpty(tagClasses);
         Assert.Contains(tagClasses, s => s == className);
     }
 }
